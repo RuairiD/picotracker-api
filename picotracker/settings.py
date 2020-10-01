cddadbaad9fb7cea56dde0036e9dc007d14f0b99
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-01 15:56:06 -0700

    RUNNING_LOCALLY is False

--- picotracker/settings.py
+++ picotracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-RUNNING_LOCALLY = True
+RUNNING_LOCALLY = False
 DEBUG = RUNNING_LOCALLY or 'test' in sys.argv
 
 # SECURITY WARNING: keep the secret key used in production secret!
