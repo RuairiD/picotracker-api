3417a60ea4f7667b3db172977956ed344291c829
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-01 17:11:39 -0700

    Moved start_scheduler import to try and fix settings related crash.

--- picotracker/wsgi.py
+++ picotracker/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from picotracker.jobs import start_scheduler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picotracker.settings')
 
 application = get_wsgi_application()
 
+from picotracker.jobs import start_scheduler
 start_scheduler()
