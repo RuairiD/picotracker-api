08d07cf9d5c19e443c67ac8a230eb3df362bdcf3
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-03 17:23:44 -0700

    Update games every 3 hours on the hour.

--- picotracker/jobs.py
+++ picotracker/jobs.py
@@ -1,4 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.combining import OrTrigger
+from apscheduler.triggers.cron import CronTrigger
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.jobstores import register_events
 from django_apscheduler.jobstores import register_job
@@ -9,7 +11,13 @@ scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
 
 
-@register_job(scheduler, 'interval', hours=6, replace_existing=True)
+every_3_hours_trigger = OrTrigger([
+    CronTrigger(hour=hour)
+    for hour in range(0, 24, 3)
+])
+
+
+@register_job(scheduler, every_3_hours_trigger)
 def update_games():
     print('Starting: update_games')
     call_command('update_games')
